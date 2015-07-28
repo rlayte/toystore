75f613eb0feaba96d2544f66a746af0815a6ff62
Rich Layte <rich.layte@gmail.com>
2015-07-28 12:13:05 -0400

    Basic getting/putting requests

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func Get(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Println("Getting")
+var data map[string]string = map[string]string{}
+
+func Get(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	key := params.ByName("key")
+	fmt.Fprint(w, data[key])
 }
 
-func Put(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Println("Putting")
+func Put(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	key := params.ByName("key")
+	value := r.FormValue("data")
+	data[key] = value
 }
 
 func main() {
@@ -21,5 +27,6 @@ func main() {
 	router.GET("/:key", Get)
 	router.POST("/:key", Put)
 
+	log.Println("Running server on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
