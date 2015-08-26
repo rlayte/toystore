8208530a9553c4421d4de4b5e0ca8ce4d902fff4
Rich Layte <rich.layte@gmail.com>
2015-08-26 13:58:30 -0400

    Add basic toystore tests

---
+++ toystore_test.go
@@ -0,0 +1,46 @@
+package toystore
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestBasicData(t *testing.T) {
+	hosts := []string{
+		"http://localhost:3000",
+		"http://localhost:3001",
+		"http://localhost:3002",
+		"http://localhost:3003",
+		"http://localhost:3004",
+	}
+
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("basic-%d", i)
+		value := fmt.Sprintf("basic-value-%d", i)
+		host := hosts[rand.Intn(len(hosts))]
+		data := url.Values{"key": {key}, "value": {value}}
+
+		_, err := http.PostForm(host, data)
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		resp, err := http.Get(host + "/" + key)
+		defer resp.Body.Close()
+
+		if err != nil {
+			t.Error(err)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if string(body) != value {
+			t.Errorf("%s should equal %s", string(body), value)
+		}
+	}
+}
