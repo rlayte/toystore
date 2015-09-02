541158ba6b8b25fc2dda3a681f315f19bd91105a
Rich Layte <rich.layte@gmail.com>
2015-09-02 11:52:17 -0400

    Make memory store thread safe

--- adapters/memory/memory.go
+++ adapters/memory/memory.go
@@ -1,23 +1,32 @@
 package memory
 
-import "github.com/rlayte/toystore/data"
+import (
+	"sync"
+
+	"github.com/rlayte/toystore/data"
+)
 
 type MemoryStore struct {
+	lock *sync.Mutex
 	data map[string]*data.Data
 }
 
 func (m MemoryStore) Get(key string) (*data.Data, bool) {
+	m.lock.Lock()
 	value, ok := m.data[key]
+	m.lock.Unlock()
 	return value, ok
 }
 
 func (m MemoryStore) Put(d *data.Data) bool {
+	m.lock.Lock()
 	m.data[d.Key] = d
+	m.lock.Unlock()
 	return true
 }
 
 func New() *MemoryStore {
-	return &MemoryStore{map[string]*data.Data{}}
+	return &MemoryStore{&sync.Mutex{}, map[string]*data.Data{}}
 }
 
 func (m MemoryStore) Keys() []string {
