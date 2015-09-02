349d7f43d1ee7920377c0e8a90e04512d4c2182b
Rich Layte <rich.layte@gmail.com>
2015-09-02 12:33:48 -0400

    Add docs for config

--- config.go
+++ config.go
@@ -2,15 +2,40 @@ package toystore
 
 import "time"
 
+// Config defines the variables used for a Toystore node.
 type Config struct {
+	// Number of nodes to store a key on.
 	ReplicationLevel int
-	W                int
-	R                int
-	ClientPort       int
-	RPCPort          int
-	GossipPort       int
-	Host             string
-	SeedAddress      string
-	Store            Store
-	HandoffInterval  time.Duration
+
+	// W is the number of times a key must be replicated for a Put operation
+	// to be successful.
+	W int
+
+	// R is the number of nodes a key must be read from for a Get operation
+	// to be successful.
+	R int
+
+	// ClientPort is the port Toystore will use to serve client requests.
+	ClientPort int
+
+	// RPCPort is the port Toystore will use for RPC between nodes.
+	RPCPort int
+
+	// GossipPort is the port Toystore will use for membership updates via
+	// its gossip protocol.
+	GossipPort int
+
+	// Host is the ip Toystore will bind to.
+	Host string
+
+	// SeedAddress is a known Toystore node that used to initial join the
+	// cluster.
+	SeedAddress string
+
+	// Store is an implementation of the Store interface the handles persisting
+	// data.
+	Store Store
+
+	// HandoffInterval is the time between scans of the hinted handoff list.
+	HandoffInterval time.Duration
 }
