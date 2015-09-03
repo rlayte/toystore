3f5ecf45f712650247ba8ad1f6082aa31de42999
Rich Layte <rich.layte@gmail.com>
2015-09-03 09:17:27 -0400

    Add docs for memory store

--- adapters/memory/memory.go
+++ adapters/memory/memory.go
@@ -1,3 +1,5 @@
+// Package memory is an example Store implementation that saves values to
+// local memory.
 package memory
 
 import (
@@ -11,6 +13,8 @@ type MemoryStore struct {
 	data map[string]*data.Data
 }
 
+// Get returns a Data value and existence bool for the given key.
+// Thread safe.
 func (m MemoryStore) Get(key string) (*data.Data, bool) {
 	m.lock.Lock()
 	value, ok := m.data[key]
@@ -18,6 +22,8 @@ func (m MemoryStore) Get(key string) (*data.Data, bool) {
 	return value, ok
 }
 
+// Put adds a new Data value and returns a success status bool.
+// Thread safe.
 func (m MemoryStore) Put(d *data.Data) bool {
 	m.lock.Lock()
 	m.data[d.Key] = d
@@ -25,16 +31,24 @@ func (m MemoryStore) Put(d *data.Data) bool {
 	return true
 }
 
-func New() *MemoryStore {
-	return &MemoryStore{&sync.Mutex{}, map[string]*data.Data{}}
-}
-
+// Keys returns a list of all keys added to the store.
+// Thread safe.
 func (m MemoryStore) Keys() []string {
 	out := make([]string, len(m.data))
 	i := 0
+
+	m.lock.Lock()
 	for key := range m.data {
 		out[i] = key
 		i++
 	}
+	m.lock.Unlock()
+
 	return out
+}
+
+// New returns a new MemoryStore instance, creating the required
+// data structure and lock.
+func New() *MemoryStore {
+	return &MemoryStore{&sync.Mutex{}, map[string]*data.Data{}}
 }
