091d592b9c13451b00441b918bd0d0bf16abbdab
Rich Layte <rich.layte@gmail.com>
2015-09-03 10:42:00 -0400

    Add docs for peer_handler

--- peer_handler.go
+++ peer_handler.go
@@ -8,6 +8,8 @@ import (
 	"github.com/rlayte/toystore/data"
 )
 
+// PeerHandler defines the methods that a node should expose to other
+// nodes in the cluster.
 type PeerHandler interface {
 	Get(args *GetArgs, reply *GetReply) error
 	Put(args *PutArgs, reply *PutReply) error
@@ -15,6 +17,8 @@ type PeerHandler interface {
 	CoordinatePut(args *PutArgs, reply *PutReply) error
 }
 
+// serve starts listening for RPC calls, and creates a new thread for
+// each incoming connection.
 func serve(address string, rpcs *rpc.Server) {
 	l, err := net.Listen("tcp", address)
 
@@ -33,40 +37,50 @@ func serve(address string, rpcs *rpc.Server) {
 	}
 }
 
+// RpcHandler implements PeerHandler using Go's RPC package.
 type RpcHandler struct {
 	store *Toystore
 }
 
+// Get looks up and item from Toystore's underlying Store data.
 func (r *RpcHandler) Get(args *GetArgs, reply *GetReply) error {
 	reply.Value, reply.Ok = r.store.Data.Get(args.Key)
 	return nil
 }
 
+// Put adds a value directly to Toystore's underlying Store data.
 func (r *RpcHandler) Put(args *PutArgs, reply *PutReply) error {
 	r.store.Data.Put(args.Value)
 	reply.Ok = true
 	return nil
 }
 
+// CoordinateGet kicks off the coordination process from a
+// non-coordinator node.
 func (r *RpcHandler) CoordinateGet(args *GetArgs, reply *GetReply) error {
 	reply.Value, reply.Ok = r.store.CoordinateGet(args.Key)
 	return nil
 }
 
+// CoordinatePut kicks off the coordination process from a
+// non-coordinator node.
 func (r *RpcHandler) CoordinatePut(args *PutArgs, reply *PutReply) error {
 	reply.Ok = r.store.CoordinatePut(args.Value)
 	return nil
 }
 
+// HintPut adds a new data hint to the node's HintedHandoff list.
 func (r *RpcHandler) HintPut(args *HintArgs, reply *HintReply) error {
 	r.store.Hints.Put(args.Data, args.Hint)
 	reply.Ok = true
 	return nil
 }
 
+// Transfer adds a set of data to the node.
 func (r *RpcHandler) Transfer(args *TransferArgs, reply *TransferReply) error {
 	ok := true
 
+	// TODO: Should use Merge here to take the latest value.
 	for _, item := range args.Data {
 		r.store.Data.Put(item)
 	}
@@ -75,6 +89,7 @@ func (r *RpcHandler) Transfer(args *TransferArgs, reply *TransferReply) error {
 	return nil
 }
 
+// NewRpcHandler returns a new RpcHandler instance and starts serving requests.
 func NewRpcHandler(store *Toystore) *RpcHandler {
 	gob.Register(data.Data{})
 	rpcs := rpc.NewServer()
