eb05dc782d0a1cfbb6f123b24ec9a144220db1f8
Rich Layte <rich.layte@gmail.com>
2015-09-04 15:58:22 -0400

    Add mock PartitionedRing

--- toystore_test.go
+++ toystore_test.go
@@ -1,6 +1,7 @@
 package toystore
 
 import (
+	"container/ring"
 	"fmt"
 	"log"
 	"math/rand"
@@ -139,6 +140,34 @@ func TestIntegration__NodeJoins(t *testing.T) {
 	}
 
 	time.Sleep(time.Second * 2)
+}
+
+type PartitionedRing struct {
+	ring *ring.Ring
+}
+
+func (r *PartitionedRing) Add(key string) {
+	r.ring.Add(key)
+}
+
+func (r *PartitionedRing) Find(key string) string {
+	return r.ring.Find(key)
+}
+
+func (r *PartitionedRing) FindN(key string, n int) map[string]string {
+	return r.ring.FindN(key, n)
+}
+
+func (r *PartitionedRing) Fail(key string) {
+	return r.ring.Fail(key)
+}
+
+func (r *PartitionedRing) Revive(key string) {
+	return r.ring.Revive(key)
+}
+
+func (r *PartitionedRing) Adjacent(a, b string) bool {
+	return r.ring.Adjacent(key)
 }
 
 func TestIntegration__Partitions(t *testing.T) {
